9e790c42665acfc2adb1ddb046585151321d0422
Listening-Raccoon <kprychlik@gmail.com>
2025-02-08 13:39:55 +0100

    Created layout for the pantry popup dialog

--- app/src/main/java/com/example/pamietajozdrowiu/CalendarActivity.java
+++ app/src/main/java/com/example/pamietajozdrowiu/CalendarActivity.java
@@ -3,7 +3,6 @@ package com.example.pamietajozdrowiu;
 import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,7 +15,6 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -158,7 +156,7 @@ public class CalendarActivity extends AppCompatActivity  implements RecyclerView
         detailsDialog = new Dialog(CalendarActivity.this);
         detailsDialog.setContentView(R.layout.instance_details_dialog);
         detailsDialog.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        detailsDialog.getWindow().setBackgroundDrawableResource(R.drawable.instance_details_dialog_bg);
+        detailsDialog.getWindow().setBackgroundDrawableResource(R.drawable.popup_dialog_background);
         detailsDialog.setCancelable(true);
 
         okButton = detailsDialog.findViewById(R.id.ok_button);
